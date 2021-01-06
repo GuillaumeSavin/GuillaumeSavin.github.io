1a23593264bb1443b150ceb3a629ded2bb8f8526
GuillaumeSavin <Sylvarant16>
2021-01-06 10:05:44 +0100

    Correct JS and html

--- ExoCarto2/js/main.js
+++ ExoCarto2/js/main.js
@@ -1,14 +1,43 @@
 function init() {
-    var geo1 = navigator.geolocation.getCurrentPosition(function (){}, function (){});
+    var geo1 = navigator.geolocation.getCurrentPosition(successGeo(), errorGeo() ());
+}
+
+function successGeo(pos) {
+    var geo1 = pos.coords;
     var long = geo1.coords.longitude;
     var lat = geo1.coords.latitude;
     var prec = geo1.coords.accuracy;
 
-    var longView = document.getElementById("long");
-    var latView = document.getElementById("lat");
-    var precView = document.getElementById("prec");
-
+    var longDiv = document.getElementById("long");
+    var longView = document.createElement("p");
     longView.innerHTML = long;
+    longDiv.appendChild(longView);
+
+    var latDiv = document.getElementById("lat");
+    var latView = document.createElement("p");
     latView.innerHTML = lat;
+    latDiv.appendChild(latView);
+
+    var precDiv = document.getElementById("prec");
+    var precView = document.createElement("p");
     precView.innerHTML = prec;
+    precDiv.appendChild(precView);
+
+}
+
+function errorGeo(err) {
+    var longDiv = document.getElementById("long");
+    var longView = document.createElement("p");
+    longView.innerHTML = 'ERREUR (${err.code}): ${err.message}';
+    longDiv.appendChild(longView);
+
+    var latDiv = document.getElementById("lat");
+    var latView = document.createElement("p");
+    latView.innerHTML = 'ERREUR (${err.code}): ${err.message}';;
+    latDiv.appendChild(latView);
+
+    var precDiv = document.getElementById("prec");
+    var precView = document.createElement("p");
+    precView.innerHTML = 'ERREUR (${err.code}): ${err.message}';;
+    precDiv.appendChild(precView);
 }
