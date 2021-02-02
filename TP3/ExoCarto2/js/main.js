65d32fcff0af4b85418907759df035ebb4736806
GuillaumeSavin <Sylvarant16>
2021-02-02 12:19:51 +0100

    test

--- TP3/ExoCarto2/js/main.js
+++ TP3/ExoCarto2/js/main.js
@@ -61,26 +61,8 @@ window.addEventListener("deviceorientation", function (event) {
 window.addEventListener("devicemotion", function (event) {
     motion.x = event.accelerationIncludingGravity.x;
     motion.y = event.accelerationIncludingGravity.y;
-    if (motion.x > 0) {
-        motion.x = 0.01;
-    } else if(motion.x === 0) {
-        motion.x = 0;
-    } else {
-        motion.x = -0.01;
-    }
+});
 
-    if (motion.y > 0) {
-        motion.y = 0.01;
-    } else if(motion.y === 0) {
-        motion.y = 0;
-    } else {
-        motion.y = -0.01;
-    }
-})
-
-
-//var myObject = scene.children[2];
-console.log(loader);
 var render = function () {
     requestAnimationFrame(render);
     cube.rotation.x = orientation.x;
